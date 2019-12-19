f3281d7c0b77e724510547bef2329dba7bb13238
Julian Gruber <julian@juliangruber.com>
2019-12-19 14:27:38 +0100

    add react-spring

--- app/index.js
+++ app/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import styled from 'styled-components'
+import { useSpring, animated } from 'react-spring'
 
-const Container = styled.div`
+const Container = styled(animated.div)`
   color: purple;
   background-color: white;
 `
 
-const App = () => <Container>Hi!</Container>
+const App = () => {
+  const props = useSpring({ opacity: 1, from: { opacity: 0 } })
+  return <Container style={props}>Hi!</Container>
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
