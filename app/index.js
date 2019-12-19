139e55a84bb8c266f29f724cb5c1aa59ce242a80
Julian Gruber <julian@juliangruber.com>
2019-12-19 14:23:56 +0100

    add styled-components

--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
 
-const App = () => <div>Hi!</div>
+const Container = styled.div`
+  color: purple;
+  background-color: white;
+`
+
+const App = () => <Container>Hi!</Container>
+
 ReactDOM.render(<App />, document.getElementById('root'))
