4537b41ad89dffcefe25a320cd9376da2066adf7
Julian Gruber <julian@juliangruber.com>
2019-12-19 14:21:09 +0100

    add @storybook/addon-knobs

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -2,23 +2,21 @@ import React from 'react'
 
 import { action } from '@storybook/addon-actions'
 import { Button } from '@storybook/react/demo'
+import { withKnobs, text } from '@storybook/addon-knobs'
 
 export default {
-  title: 'Button'
+  title: 'Button',
+  decorators: [withKnobs]
 }
 
-export const text = () => (
-  <Button onClick={action('clicked')}>Hello Button</Button>
+export const withText = () => (
+  <Button onClick={action('clicked')}>{text('Label', 'Hello Button')}</Button>
 )
 
-export const emoji = () => (
+export const withEmoji = () => (
   <Button onClick={action('clicked')}>
     <span role='img' aria-label='so cool'>
       😀 😎 👍 💯
     </span>
   </Button>
 )
-
-emoji.story = {
-  name: 'with emoji'
-}
