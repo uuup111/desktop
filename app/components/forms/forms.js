0d61a6af7808e23dfa19e09546c86cacff9ec7f2
Julian Gruber <julian@juliangruber.com>
2020-03-31 14:56:48 +0200

    finish select design. #47

--- app/components/forms/forms.js
+++ app/components/forms/forms.js
@@ -1,5 +1,8 @@
+import React from 'react'
 import styled from 'styled-components'
 import { purple, black, white } from '../../lib/colors'
+import CaretDown from '../carets/caret-down-1rem.svg'
+import CaretDownLarge from '../carets/caret-down-2rem.svg'
 
 export const Label = styled.label`
   font-weight: bold;
@@ -18,20 +21,43 @@ export const Input = styled.input`
   box-sizing: border-box;
   font-family: Roboto;
 `
-export const Select = styled.select`
+
+const SelectContainer = styled.div`
+  position: relative;
+`
+const SelectElement = styled.select`
   display: block;
   background-color: ${black};
   color: ${white};
-  border-color: ${purple};
+  border: 2px solid ${purple};
   font-size: ${props => (props.large ? 2 : 1)}rem;
   width: 100%;
   max-width: 661px;
   height: ${props => (props.large ? 4 : 2)}rem;
-  /* border-radius: 0;
-  appearance: none; */
+  border-radius: 0;
+  appearance: none;
   margin-bottom: 1rem;
   font-family: Roboto;
+  padding-left: 19px;
+  letter-spacing: 0.05em;
 `
+const SelectCaret = styled(CaretDown)`
+  position: absolute;
+  right: 16.37px;
+  top: 5px;
+`
+const SelectCaretLarge = styled(CaretDownLarge)`
+  position: absolute;
+  right: 16.37px;
+  top: 10px;
+`
+export const Select = ({ large, ...props }) => (
+  <SelectContainer>
+    <SelectElement large={large} {...props} />
+    {large ? <SelectCaretLarge /> : <SelectCaret />}
+  </SelectContainer>
+)
+
 export const Textarea = styled.textarea`
   border: 2px solid ${purple};
   background-color: ${black};
